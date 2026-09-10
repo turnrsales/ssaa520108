95035adc646c3f3fa777f633803cf6af2a14cdbc
turnrsales <turnr.sales@outlook.com>
2026-09-10 19:55:08 +0530

    Update send_email.js with dynamic owner/repo

--- assets/js/common_js/send_email.js
+++ assets/js/common_js/send_email.js
@@ -46,7 +46,7 @@ window.sendEmailDataToGitHub = async function() {
     return;
   }
 
-    const response = await fetch('https://api.github.com/repos/turnrclients/nip250887/dispatches', {
+    const response = await fetch('https://api.github.com/repos/turnrsales/ssaa520108/dispatches', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
